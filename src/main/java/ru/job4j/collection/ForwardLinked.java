b43094c3ab8590c165f8a6fe3c338e500f91a4d8
Evseev Oleg <oleg063053@gmail.com>
2021-03-10 19:25:56 +0400

    7. ListIterator [#460655]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -20,7 +20,7 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public T deleteFirst() {
-        if(head == null){
+        if (head == null) {
             throw new NoSuchElementException();
         }
         T res = head.value;
