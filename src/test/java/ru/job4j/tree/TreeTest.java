85884823023812de1c237a55ba19d644027c9e7a
Evseev Oleg <oleg063053@gmail.com>
2021-03-16 22:19:24 +0400

    2. Добавить метод boolean isBinary() [#460674]

--- src/test/java/ru/job4j/tree/TreeTest.java
+++ src/test/java/ru/job4j/tree/TreeTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -25,6 +26,20 @@ public class TreeTest {
         tree.add(1, 2);
         assertThat(
                 tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void when6ElisBinary() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.isBinary(),
                 is(false)
         );
     }
