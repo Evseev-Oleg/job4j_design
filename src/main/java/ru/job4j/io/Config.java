988ff39e276f25f56df6a14c73eb235d2e6f8e42
Evseev Oleg <oleg063053@gmail.com>
2021-03-25 20:34:06 +0400

    1. Читаем файл конфигурации [#460710]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -2,23 +2,39 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
 
 public class Config {
     private final String path;
-    private final Map<String, String> values = new HashMap<String, String>();
+    private final Map<String, String> values = new HashMap<>();
 
     public Config(final String path) {
         this.path = path;
     }
 
     public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            for (String line = read.readLine(); line != null; line = read.readLine()) {
+                String[] keyAndValue = line.split("=");
+                if (keyAndValue[0].contains("#") || keyAndValue[0].isEmpty()) {
+                    continue;
+                }
+                if (keyAndValue.length != 2) {
+                    throw new IllegalArgumentException();
+                }
+                String[] value = keyAndValue[1].split("//");
+                values.put(keyAndValue[0], value[0]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String value(String key) {
-        throw new UnsupportedOperationException("Don't impl this method yet!");
+        return values.get(key);
     }
 
     @Override
--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -16,4 +16,44 @@ public class ConfigTest {
                 is("Petr Arsentev")
         );
     }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Petr Arsentev")
+        );
+    }
+
+    @Test
+    public void whenPairWithTwoComment() {
+        String path = "./data/pair_with_two_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Petr Arsentev")
+        );
+    }
+
+    @Test
+    public void whenEmptyString() {
+        String path = "./data/pair_with_empty_string.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Petr Arsentev")
+        );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenException() {
+        String path = "./data/exception.properties";
+        Config config = new Config(path);
+        config.load();
+    }
 }
