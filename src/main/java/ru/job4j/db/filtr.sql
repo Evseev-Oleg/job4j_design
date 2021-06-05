c4d3100b560fe20e2e23ad90c3730fa9fa12d6d0
Evseev Oleg <oleg063053@gmail.com>
2021-06-05 21:43:50 +0400

    Фильтры [#1732]

---
+++ src/main/java/ru/job4j/db/filtr.sql
@@ -0,0 +1,7 @@
+select product.name from product, type where type.name = 'сыр' and type.id = product.type_id;
+select * from product where name like '%мороженное%';
+select * from product where expired_date < current_date;
+select max(price) from product;
+select t.name,count(p.name) from type as t,product as p where t.id = p.type_id group by t.name;
+select * from product, type where (type.name = 'сыр' or type.name = 'молоко') and type.id = product.type_id;
+select t.name from type as t,product as p where p.quantity < 10 group by t.name;
