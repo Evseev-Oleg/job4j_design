d274d8de6e3058212339e520608b1d8b0a1fc65d
Evseev Oleg <oleg063053@gmail.com>
2021-06-16 01:22:42 +0400

    0. JDBC [#6863 #167097]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,28 +1,19 @@
 package ru.job4j.jdbc;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import ru.job4j.io.Config;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config("src/main/resources/app.properties");
         Class.forName("org.postgresql.Driver");
-        Properties property = new Properties();
-        String url = null;
-        String login = null;
-        String password = null;
-        try (FileInputStream fis = new FileInputStream("src/main/resources/app.properties")){
-            property.load(fis);
-            url = property.getProperty("db.url");
-            login = property.getProperty("db.login");
-            password = property.getProperty("db.password");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String url = config.value("db.url");
+        String login = config.value("db.login");
+        String password = config.value("db.password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
